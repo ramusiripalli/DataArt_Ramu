0bffd2375f886d04091f34a4240ed4d5051096a0
ramu siripalli <ramsiripalli2772@gmail.com>
2025-08-23 13:17:51 +0530

    Every Code is Structured According to its Folder for better Visibility

--- TimeLineReactPlusAccessibility/src/components/FilterPanel.tsx
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line react-refresh/only-export-components
-
-import React from "react";
-
-const FilterPanel: React.FC = () => {
-  return <div className="filter-panel">[Filters coming soon]</div>;
-};
-
-export default FilterPanel;
